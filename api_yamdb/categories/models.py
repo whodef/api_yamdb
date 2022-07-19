017912d5e698a9502a8997f8efc27820ab4ba8fb
Максим Чен <chenmaxim@gmail.com>
2022-07-20 00:19:50 +0500

    fix: Created models

--- api_yamdb/categories/models.py
+++ api_yamdb/categories/models.py
@@ -1,17 +1,33 @@
 from django.db import models
 
+from reviews.models import Review
+
 
 class Genre(models.Model):
-    name = models.CharField(max_length=32, verbose_name='Название жанра')
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=256, verbose_name='Название жанра')
+    slug = models.SlugField(
+        max_length=50, verbose_name='Слаг-название жанра', unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=32, verbose_name='Название категории')
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=256, verbose_name='Название категории')
+    slug = models.SlugField(
+        max_length=50, verbose_name='Слаг-название категории', unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -23,18 +39,46 @@ class Title(models.Model):
     )
     year = models.IntegerField(verbose_name='Год')
     description = models.TextField(verbose_name='Описание')
-    genre = models.ManyToManyField(Genre, through='TitleGenre')
-    category = models.ForeignKey(
-        Category, on_delete=models.DO_NOTHING, related_name='title'
+    rating = models.ForeignKey(
+        Review,
+        on_delete=models.SET_NULL,
+        related_name='title',
+        verbose_name='Отзыв о произведении',
+        null=True,
     )
+    genre = models.ManyToManyField(
+        Genre, through='TitleGenre', verbose_name='Жанр произведения'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='title',
+        verbose_name='Категория произведения',
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
 
 
 class TitleGenre(models.Model):
-    title = models.ForeignKey(Title, on_delete=models.DO_NOTHING)
-    genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING)
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.SET_NULL,
+        verbose_name='Произведение',
+        null=True,
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        verbose_name='Жанр произведения',
+        null=True,
+    )
 
     def __str__(self):
         return f'{self.genre} {self.title}'
