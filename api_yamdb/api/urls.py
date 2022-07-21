c238e673577b3947db02bed5e510d2997bd09dbc
Максим Чен <chenmaxim@gmail.com>
2022-07-21 20:13:30 +0500

    feat: Created viewsets title/genre/category

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from reviews.views import ReviewViewSet, CommentViewSet
+from categories.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 
 router = DefaultRouter()
@@ -7,3 +8,6 @@ router.register(r'titles/(?P<title_id>\d+)/reviews',
                 ReviewViewSet, basename='reviews')
 router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
                 r'/comments', CommentViewSet, basename='comments')
+router.register(r'titles', TitleViewSet, basename='titles')
+router.register(r'genres', GenreViewSet, basename='genres')
+router.register(r'categories', CategoryViewSet, basename='categories')
--- api_yamdb/categories/views.py
+++ api_yamdb/categories/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from rest_framework import viewsets
 
-# Create your views here.
+from categories.models import Category, Genre, Title
+from api.serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer,
+)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    serializer_class = TitleSerializer 
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_queryset(self):
+        return Title.objects.annotate(
+            rating=Avg('reviews__rating')
+        ).order_by('name')
+        
