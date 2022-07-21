f315b96df793a79b6730df1c019a5cf142fb5db1
whodef <tatyana-selyuk@yandex.com>
2022-07-21 11:41:12 +0300

    feat: User's permissions

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,9 +4,9 @@ from django.utils.translation import gettext_lazy as gtl
 
 
 CHOICES = [
-    ('user', 'Пользователь'),
-    ('moderator', 'Модератор'),
-    ('admin', 'Администратор'),
+        ('USER', 'пользователь'),
+        ('MODERATOR', 'модератор'),
+        ('ADMIN', 'Администратор'),
 ]
 
 
@@ -16,22 +16,35 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['email']
 
     first_name = models.CharField(
-        gtl('first_name'), max_length=150,
+        gtl('first_name'),
+        max_length=150,
     )
     last_name = models.CharField(
-        gtl('last_name'), max_length=150, blank=True,
+        gtl('last_name'),
+        max_length=150,
+        blank=True,
     )
     email = models.EmailField(
-        gtl('email_address'), max_length=254, unique=True,
+        gtl('email_address'),
+        max_length=254,
+        unique=True,
     )
     password = models.CharField(
-        'Пароль', max_length=128, blank=False, null=True,
+        'Пароль',
+        max_length=128,
+        blank=False,
+        null=True,
     )
     bio = models.TextField(
-        'Биография', blank=True, null=True
+        'Биография',
+        blank=True,
+        null=True
     )
     role = models.CharField(
-        'Роль', max_length=42, choices=CHOICES, default=CHOICES[0][0],
+        'Роль',
+        max_length=42,
+        choices=CHOICES,
+        default=CHOICES[0][0],
     )
     confirmation_code = models.CharField(max_length=16, blank=True)
 
