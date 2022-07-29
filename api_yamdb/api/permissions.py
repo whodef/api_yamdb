5480b9c2f876df68d73c5525e1ef1939d4996c3e
whodef <tatyana-selyuk@yandex.com>
2022-07-29 18:44:04 +0300

    fix: Fixes after first review

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,10 +19,10 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user
-                and request.user.is_authenticated
-                and request.user.is_admin
+            request.method in permissions.SAFE_METHODS
+            or request.user
+            and request.user.is_authenticated
+            and request.user.is_admin
         )
 
 
@@ -33,16 +33,16 @@ class IsAdminModeratorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_admin
-                or request.user.is_moderator
-                or obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_admin
+            or request.user.is_moderator
+            or obj.author == request.user
         )
 
 
@@ -53,8 +53,8 @@ class IsAdminSuperuserOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or (not request.user.is_anonymous
-                    and request.method in User.admin_methods
-                    and (request.user.is_superuser or request.user.is_admin))
+            request.method in permissions.SAFE_METHODS
+            or (not request.user.is_anonymous
+                and request.method in User.admin_methods
+                and (request.user.is_superuser or request.user.is_admin))
         )
