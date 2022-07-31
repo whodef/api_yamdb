68f6978cc76f1993f67f3aaa9559623d963f3dcb
whodef <tatyana-selyuk@yandex.com>
2022-07-31 15:08:32 +0300

    fix: Fixes after second review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,20 +7,20 @@ from reviews.views import ReviewViewSet, CommentViewSet
 from categories.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 
-router = SimpleRouter()
-router.register(r'users', AdminViewSet, basename='users')
+router_v1 = SimpleRouter()
+router_v1.register(r'users', AdminViewSet, basename='users')
 
-router.register(r'titles', TitleViewSet, basename='titles')
-router.register(
+router_v1.register(r'titles', TitleViewSet, basename='titles')
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)' r'/comments',
     CommentViewSet,
     basename='comments',
 )
-router.register(r'genres', GenreViewSet, basename='genres')
-router.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'genres', GenreViewSet, basename='genres')
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
 
 urlpatterns = [
     path('v1/auth/token/', get_token, name='get_token'),
@@ -29,5 +29,5 @@ urlpatterns = [
     ),
     path('v1/auth/signup/', RegisterView.as_view(), name='auth_register'),
     path('v1/users/me/', UserView.as_view(), name='user_me'),
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
